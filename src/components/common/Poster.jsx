798a2b6fafec0d65379deaa3ec0d55ee6b4f378a
MinJeong1221 <mjeong1998@naver.com>
2023-07-21 01:57:28 +0900

    사용안하는것 삭제

--- src/components/common/Poster.jsx
+++ src/components/common/Poster.jsx
@@ -2,60 +2,59 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 function Poster({ data }) {
-    const {
-        id,
-        title,
-        release_date,
-        poster_path,
-        name,
-        first_air_date,
-        media_type,
-        profile_path,
-    } = data;
+  const {
+    id,
+    title,
+    release_date,
+    poster_path,
+    name,
+    first_air_date,
+    profile_path,
+  } = data;
 
-    const src =
-        "https://image.tmdb.org/t/p/w154/" + (poster_path ?? profile_path);
+  const src =
+    "https://image.tmdb.org/t/p/w154/" + (poster_path ?? profile_path);
 
-    // release_date가 있으면 movie, first_air_date 가 있으면 tv,  둘다 없으면 person => 삼항연산자 중첩
-    const to = release_date ? "movie" : first_air_date ? "tv" : "person";
+  // release_date가 있으면 movie, first_air_date 가 있으면 tv,  둘다 없으면 person => 삼항연산자 중첩
+  const to = release_date ? "movie" : first_air_date ? "tv" : "person";
 
-    return (
-        <Container>
-            <Link to={`/${to}/${id}`}>
-                <ImgBox>
-                    <img src={src} alt="" />
-                </ImgBox>
-                <TitleBox>
-                    <span>{release_date || first_air_date}</span>
-                    <h4>{title || name}</h4>
-                </TitleBox>
-            </Link>
-        </Container>
-    );
+  return (
+    <Container>
+      <Link to={`/${to}/${id}`}>
+        <ImgBox>
+          <img src={src} alt="" />
+        </ImgBox>
+        <TitleBox>
+          <span>{release_date || first_air_date}</span>
+          <h4>{title || name}</h4>
+        </TitleBox>
+      </Link>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    border-radius: 10px;
-    overflow: hidden;
-    background-color: #fff;
+  border-radius: 10px;
+  overflow: hidden;
+  background-color: #fff;
 
-    box-shadow: 4px 4px 4px 0 rgba(0, 0, 0, 0.3);
+  box-shadow: 4px 4px 4px 0 rgba(0, 0, 0, 0.3);
 `;
 
 const ImgBox = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 180px;
-    overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 180px;
+  overflow: hidden;
 `;
 
 const TitleBox = styled.div`
-    height: 70px;
-    padding: 0 10px;
-    span {
-        font-size: 14px;
-    }
+  height: 70px;
+  padding: 0 10px;
+  span {
+    font-size: 14px;
+  }
 `;
 
 export default Poster;
--- src/components/home/SearchBox.jsx
+++ src/components/home/SearchBox.jsx
@@ -1,42 +1,42 @@
 import styled from "styled-components";
 import Button from "../common/button";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 function SearchBox() {
-    const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        navigate("/search?query=" + query);
-    };
+  const handleSubmit = e => {
+    e.preventDefault();
+    navigate("/search?query=" + query);
+  };
 
-    return (
-        <Container>
-            <form onSubmit={handleSubmit}>
-                <input type="text" onChange={(e) => setQuery(e.target.value)} />
-                <Button>검색</Button>
-            </form>
-        </Container>
-    );
+  return (
+    <Container>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={e => setQuery(e.target.value)} />
+        <Button>검색</Button>
+      </form>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    form {
-        display: flex;
-    }
+  form {
+    display: flex;
+  }
 
-    input {
-        flex: 1;
-        padding: 5px 10px;
-        margin-right: 10px;
-        border-radius: 10px;
-        border: 1px solid #bbb;
-        outline: none;
-    }
-    margin-bottom: 20px;
+  input {
+    flex: 1;
+    padding: 5px 10px;
+    margin-right: 10px;
+    border-radius: 10px;
+    border: 1px solid #bbb;
+    outline: none;
+  }
+  margin-bottom: 20px;
 `;
 
 export default SearchBox;
